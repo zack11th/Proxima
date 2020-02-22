e09e03d56253d74906fc8378d6ba406d06901bb5
zack11th <zack11th@gmail.com>
2020-02-22 18:58:33 +0600

    исправлены АЛЕРТЫ у МАСТЕРА

--- client/src/components/Master.vue
+++ client/src/components/Master.vue
@@ -6,23 +6,23 @@
       <hr>
       <!--************ALERTS***********-->
       <h2>ALERTS</h2>
-      <div class="flex">
+      <div class="flex no-wrap">
         <!--****** COMMANDER ALERTS ***********-->
         <div class="alert">
           <h2>Commander Alerts</h2>
-          <div>
+          <div class="input-block">
             <label>header</label>
             <input type="text" v-model="alertCommand.header">
           </div>
-          <div>
+          <div class="input-block">
             <label>message</label>
             <textarea v-model="alertCommand.message"></textarea>
           </div>
-          <div>
+          <div class="input-block">
             <label>button</label>
             <input type="checkbox" v-model="alertCommand.button">
           </div>
-          <div class="alert-in-process" v-if="alertInProcess_commander">ALERT</div>
+          <div class="alert-in-process" v-if="alertInProcess_commander">{{alertMessage_commander}}</div>
           <div class="action">
             <button @click="clearAlert(alertCommand, alertEvents.commander)">Clear</button>
             <button @click="clickAlert(alertCommand, alertEvents.commander)">Отправить</button>
@@ -31,19 +31,19 @@
         <!--******PILOT ALERTS ***********-->
         <div class="alert">
           <h2>Pilot Alerts</h2>
-          <div>
+          <div class="input-block">
             <label>header</label>
             <input type="text" v-model="alertPilot.header">
           </div>
-          <div>
+          <div class="input-block">
             <label>message</label>
             <textarea v-model="alertPilot.message"></textarea>
           </div>
-          <div>
+          <div class="input-block">
             <label>button</label>
             <input type="checkbox" v-model="alertPilot.button">
           </div>
-          <div class="alert-in-process" v-if="alertInProcess_pilot">ALERT</div>
+          <div class="alert-in-process" v-if="alertInProcess_pilot">{{alertMessage_pilot}}</div>
           <div class="action">
             <button @click="clearAlert(alertPilot, alertEvents.pilot)">Clear</button>
             <button @click="clickAlert(alertPilot, alertEvents.pilot)">Отправить</button>
@@ -52,19 +52,19 @@
         <!--******ENGINEER ALERTS ***********-->
         <div class="alert">
           <h2>Engineer Alerts</h2>
-          <div>
+          <div class="input-block">
             <label>header</label>
             <input type="text" v-model="alertEng.header">
           </div>
-          <div>
+          <div class="input-block">
             <label>message</label>
             <textarea v-model="alertEng.message"></textarea>
           </div>
-          <div>
+          <div class="input-block">
             <label>button</label>
             <input type="checkbox" v-model="alertEng.button">
           </div>
-          <div class="alert-in-process" v-if="alertInProcess_engineer">ALERT</div>
+          <div class="alert-in-process" v-if="alertInProcess_engineer">{{alertMessage_engineer}}</div>
           <div class="action">
             <button @click="clearAlert(alertEng, alertEvents.engineer)">Clear</button>
             <button @click="clickAlert(alertEng, alertEvents.engineer)">Отправить</button>
@@ -73,19 +73,19 @@
         <!--******MEDIC ALERTS ***********-->
         <div class="alert">
           <h2>Medic Alerts</h2>
-          <div>
+          <div class="input-block">
             <label>header</label>
             <input type="text" v-model="alertMedic.header">
           </div>
-          <div>
+          <div class="input-block">
             <label>message</label>
             <textarea v-model="alertMedic.message"></textarea>
           </div>
-          <div>
+          <div class="input-block">
             <label>button</label>
             <input type="checkbox" v-model="alertMedic.button">
           </div>
-          <div class="alert-in-process" v-if="alertInProcess_medic">ALERT</div>
+          <div class="alert-in-process" v-if="alertInProcess_medic">{{alertMessage_medic}}</div>
           <div class="action">
             <button @click="clearAlert(alertMedic, alertEvents.medic)">Clear</button>
             <button @click="clickAlert(alertMedic, alertEvents.medic)">Отправить</button>
@@ -94,19 +94,19 @@
         <!--******GENERAL ALERTS ***********-->
         <div class="alert">
           <h2>Общий Alerts</h2>
-          <div>
+          <div class="input-block">
             <label>header</label>
             <input type="text" v-model="alertGeneral.header">
           </div>
-          <div>
+          <div class="input-block">
             <label>message</label>
             <textarea v-model="alertGeneral.message"></textarea>
           </div>
-          <div>
+          <div class="input-block">
             <label>button</label>
             <input type="checkbox" v-model="alertGeneral.button">
           </div>
-          <div class="alert-in-process" v-if="alertInProcess_general">ALERT</div>
+          <div class="alert-in-process" v-if="alertInProcess_general">{{alertMessage_general}}</div>
           <div class="action">
             <button @click="clearAlert(alertGeneral, alertEvents.general)">Clear</button>
             <button @click="clickAlert(alertGeneral, alertEvents.general)">Отправить</button>
@@ -116,9 +116,12 @@
       <!--**********end ALERTS*********-->
       <hr>
       <div class="flex">
+        <!--************** PILOT *************-->
         <div class="pilot flex-row">
           <h3>Пилоты</h3>
+
         </div>
+        <!--************** end PILOT ************-->
         <div class="scientist flex-row">
           <h3>Ученый</h3>
         </div>
@@ -192,6 +195,21 @@
         },
         alertInProcess_general() {
           return this.$store.getters.alertGeneral.inProcess
+        },
+        alertMessage_pilot() {
+          return this.$store.getters.alertPilot.message
+        },
+        alertMessage_engineer() {
+          return this.$store.getters.alertEng.message
+        },
+        alertMessage_medic() {
+          return this.$store.getters.alertMedic.message
+        },
+        alertMessage_commander() {
+          return this.$store.getters.alertCommand.message
+        },
+        alertMessage_general() {
+          return this.$store.getters.alertGeneral.message
         }
       },
       methods: {
@@ -248,11 +266,14 @@
     display: flex;
     flex-wrap: wrap;
   }
+  .flex.no-wrap {
+    flex-wrap: nowrap;
+  }
   .flex-row {
     border-right: 2px solid white;
     width: 25%;
   }
-  .flex > div > div {
+  .input-block {
     display: flex;
     justify-content: space-between;
   }
--- server/src/controllers/alerts.js
+++ server/src/controllers/alerts.js
@@ -2,7 +2,21 @@ function alerts(io, socket) {
 
     socket.on('alert', data => {
         io.emit(data.event, data.alert);
-    })
+    });
+
+    socket.on('startLarp', () => {
+        alert = {
+            header: 'Внимание!',
+            message: 'Автоматические системы прокладки курса отключены. Зафиксирована аномалия: черная дыра. Вероятность столкновения в автоматическом режиме 100%. Необходимо проложить курс в ручном режиме. Активируйте ручное управление ядерным двигателем.',
+            button: true,
+            inProcess: true
+        };
+        io.emit('alertPilot', alert);
+        io.emit('alertCommand', alert);
+        io.emit('alertGeneral', alert);
+    });
+
+
 }
 
 module.exports = alerts;
